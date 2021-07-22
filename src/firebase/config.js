cb9d17ad503f02061384e802bbc7f145536a4d77
xirade <6myusic6@gmail.com>
2021-07-22 21:29:38 +0300

    update gitignore

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -6,10 +6,10 @@ import "firebase/functions";
 
 const firebaseConfig = {
   apiKey: process.env.VUE_APP_API_KEY,
-  authDomain: "ximail.firebaseapp.com",
-  projectId: "ximail",
-  storageBucket: "ximail.appspot.com",
-  messagingSenderId: "752082075882",
+  authDomain: process.env.VUE_APP_AUTH_DOMAIN,
+  projectId: process.env.VUE_APP_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID,
   appId: process.env.VUE_APP_ID
 };
 
